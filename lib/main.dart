04d9c98039319fd3a59ea25dde277c83150382d7
artemdiashkin <nemrosim1988@gmail.com>
2023-03-27 23:17:54 +0300

    add timer page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'bloc/counter_cubit.dart';
-import 'bloc/counter_observer.dart';
+import 'bloc/counter/counter_cubit.dart';
+import 'bloc/counter/counter_observer.dart';
 import 'pages/Counter/counter.dart';
 
 void main() {
--- lib/pages/counter/counter.dart
+++ lib/pages/counter/counter.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../bloc/counter_cubit.dart';
+import '../../bloc/counter/counter_cubit.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({super.key});
